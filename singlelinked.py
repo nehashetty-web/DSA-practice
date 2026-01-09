5b31052df85b97b9bafd764d5866d0f124f978dd
nehashetty-web <nehasree542@gmail.com>
2026-01-09 22:37:22 +0530

    Create singlelinked.py

---
+++ singlelinked.py
@@ -0,0 +1,5 @@
+'''Write a program to create a singly linked list and display all its elements.
+Write a program to insert a new node at the beginning of a singly linked list.
+Write a program to delete the last node from a singly linked list.
+Write a program to search for a given element in a singly linked list and display 
+whether it is found or not.'''
