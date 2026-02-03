39226f3ef7eacc852555cd7b435466023a36ff71
nehashetty-web <nehasree542@gmail.com>
2026-02-03 22:27:31 +0530

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,6 @@
+'''Write a program to check whether an expression has balanced parentheses using stack.
+Write a program to reverse a given string using stack.
+Write a program to evaluate a postfix expression using stack.
+Write a program to find the next greater element for each element in an array using stack.
+Write a program to design a stack that supports push, pop, and getMin() in O(1) time.'''
+"solutions"
