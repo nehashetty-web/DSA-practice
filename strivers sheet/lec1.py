a48ba7aa5acad54894f336f2b4a8d230c36c1f89
nehashetty-web <nehasree542@gmail.com>
2026-04-13 07:29:11 +0530

    Create lec1.py

---
+++ strivers sheet/lec1.py
@@ -0,0 +1,40 @@
+#input logic
+name=input("enter your name:")
+age=int(input("enter your age:"))
+more=100-age
+print("you will be 100 years in " ,more ,"years")
+#datatypes
+n=int(input("enter n"))
+x=float(input("enter x"))
+result=n+x
+print(result)
+print(type(result))
+#if-else
+num=int(input("enter num:"))
+
+if num%5==0 and num%3==0:
+    print("FizzBuzz")
+elif num %3==0:
+    print("Fizz")
+elif num%5==0:
+    print("Buzz")   
+else:
+    print(num)
+# switch case (Python version)
+n1 = int(input("enter number 1: "))
+n2 = int(input("enter number 2: "))
+operator = input("enter operation (+, -, *, /): ")
+
+if operator == "+":
+    print(n1 + n2)
+elif operator == "-":
+    print(n1 - n2)
+elif operator == "*":
+    print(n1 * n2)
+elif operator == "/":
+    if n2 != 0:
+        print(n1 / n2)
+    else:
+        print("cannot divide by zero")
+else:
+    print("invalid operator")
