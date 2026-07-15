2d9643263556d7c385cd49eccab8ca7253556fe8
nehashetty-web <nehasree542@gmail.com>
2026-07-15 22:22:53 +0530

    Update sliding_window.py

--- DSA/sliding_window.py
+++ DSA/sliding_window.py
@@ -1,6 +1,7 @@
-#sliding window
+#sliding window fixed K
 arr=[1,2,1,5,3,1,4,2]
 k=3
+window_sum = sum(arr[:k])
 max_sum=window_sum
 for i in range(k,len(arr)):
     window_sum=window_sum-arr[i-k]+arr[i]
@@ -8,4 +9,16 @@ for i in range(k,len(arr)):
         max_sum=window_sum
 
 print(max_sum)
+
+#average size K
+arr=[1,2,1,5,3,1,4,2]
+k=3
+window_sum = sum(arr[:k])
+result=[]
+result.append(window_sum/k)
+for i in range(k,len(arr)):
+    window_sum=window_sum-arr[i-k]+arr[i]
+
+result.append(window_sum/k)
+print(result)
     
