3d772afbf53b5d83bb8f3a71ef5d8356bd547600
nehashetty-web <nehasree542@gmail.com>
2026-09-04 07:05:33 +0530

    Update binary.py

--- DSA/binary.py
+++ DSA/binary.py
@@ -11,6 +11,8 @@ while left<=right:
         left=mid+1
     else:
         right=mid-1
+
+
 #first occ
 arr = [2, 4, 6, 8, 10, 12, 14]
 target = 12
@@ -26,6 +28,8 @@ while left<=right:
         left=mid+1
     else:
         right=mid-1
+
+
 #secondf occ
 
 arr = [1, 2, 2, 2, 4, 5]
@@ -49,3 +53,80 @@ while left <= right:
         right = mid - 1
 
 print(answer)
+
+#lower bound
+arr = [1, 2, 2, 2, 4, 5]
+target = 2
+
+left = 0
+right = len(arr) - 1
+answer = -1
+
+while left <= right:
+    mid = (left + right) // 2
+
+    if arr[mid] >=target:
+        answer = mid
+        left = mid + 1
+    else:
+        right=mid-1
+
+
+#upper bound
+arr = [1, 2, 2, 2, 4, 5]
+target = 2
+
+left = 0
+right = len(arr) - 1
+answer = -1
+
+while left <= right:
+    mid = (left + right) // 2
+
+    if arr[mid] >target:
+        answer = mid
+        left = mid + 1
+    else:
+        right=mid-1
+
+#sorted array
+arr = [4, 5, 6, 7, 0, 1, 2]
+target = 0
+
+left = 0
+right = len(arr) - 1
+
+while left <= right:
+
+    # find middle
+    mid = (left + right) // 2
+
+    # target found
+    if arr[mid] == target:
+        print(mid)
+        break
+
+    # left half is sorted
+    if arr[left] <= arr[mid]:
+
+        # target is in left half
+        if arr[left] <= target < arr[mid]:
+            right = mid - 1
+
+        # target is in right half
+        else:
+            left = mid + 1
+
+    # right half is sorted
+    else:
+
+        # target is in right half
+        if arr[mid] < target <= arr[right]:
+            left = mid + 1
+
+        # target is in left half
+        else:
+            right = mid - 1
+
+else:
+    print(-1)
