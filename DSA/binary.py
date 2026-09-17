d2529d1e6672b23a1fdc8875e578390a0665759c
nehashetty-web <nehasree542@gmail.com>
2026-09-17 22:24:03 +0530

    Update binary.py

--- DSA/binary.py
+++ DSA/binary.py
@@ -178,3 +178,15 @@ while left<=right:
         print(mid)
         break
         if arr[left]<=arr[mid]:
+            if arr[left]<=target<arr[mid]:
+             right=mid-1
+            else:
+              left=mid+1
+    else:
+        if arr[mid]<=target<arr[right]:
+         left=mid+1
+        else:
+            right=mid-1
+else:
+    print(-1)
+
